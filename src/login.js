f3c46c2c2ad002921f4a8f3adad3ce6cc5ce1d67
therealhesham <heshammoha231992@gmail.com>
2023-02-03 22:37:07 +0200

    hesham

--- src/login.js
+++ src/login.js
@@ -47,7 +47,7 @@ const Poster   =()=>{
 
 //   })
   .then
-(e=>  e.data.data == "dataNotFound" ? setError("user not reistered"): console.log(e.data)
+(e=>  e.data.data == "dataNotFound" ? setError("user not reistered"): console.log(e.headers)
 )
 
 }
