774164028cc7e7a0ce609008475afcf3946f8cb6
therealhesham <heshammoha231992@gmail.com>
2023-02-10 21:40:47 +0200

    hesham

--- src/App.jsx
+++ src/App.jsx
@@ -31,8 +31,8 @@ React.useEffect(()=>
         // console.log(token)
         // if(ref.current == 0){
           
-        axios.get("https://amaccompany.onrender.com/info/"+token.id).then(e=>e.data?console.log(e.data):navigate("/login"))
-        console.log(data)
+        axios.get("https://amaccompany.onrender.com/info/"+token.id).then(e=>e.data?setData(e.data):navigate("/login"))
+        console.log(profiledata)
       // ref.current = ref.current +1
       // }
         }else if(localStorage.getItem("token") == null)
