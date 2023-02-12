76246a7da2cd78f66007cae8f7f1120d2632e46c
therealhesham <heshammoha231992@gmail.com>
2023-02-12 11:14:16 +0200

    add

--- src/datapreview.js
+++ src/datapreview.js
@@ -28,7 +28,7 @@ useEffect(()=>{
   
   if(ref.current == 0){ 
     console.log(ref.current);
-    axios.get("https://amaccompany.onrender.com/preview",{withCredentials:true}).then((e) => 
+    axios.get("https://amaccompany.onrender.com/preview").then((e) => 
     // console.log(e)
      e.data == "not authenticated" ?navigate("/login") :setSearcher(e.data) & setData(e.data) 
      )
