8e25045caa39e5a4239f764754bf809d32f0f17e
therealhesham <heshammoha231992@gmail.com>
2023-02-28 10:23:19 +0200

    hesham

--- src/components/fourth.jsx
+++ src/components/fourth.jsx
@@ -43,7 +43,7 @@ const PostHandler= ()=>{
     if (!contractor ||  !destination || !quantity || !type || !items ) return setExistense("رجاء ملىء البيانات")
 axios.post("https://amaccompany.onrender.com/refund",{receiptno:receiptno,contractor:contractor,destination:destination,itens:items,
     quantity:quantity,type:type},{withCredentials:true}).then(e=>{
-        !e.data ? setExistense("خطأ في التسجيل ... المهام غير متاحة بالمخزن") : setDone("تم تسجيل البيانات بنجاح")})
+        e.data == "error" ? setExistense("خطأ في التسجيل ... المهام غير متاحة بالمخزن") : setDone("تم تسجيل البيانات بنجاح")})
 
 
 }
