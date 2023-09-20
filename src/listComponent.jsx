81b6a3c14d6795c2cf79ba5d095c68059ee1c501
therealhesham <heshammoha231992@gmail.com>
2023-09-20 23:29:21 +0300

    commit on master

--- src/listComponent.jsx
+++ src/listComponent.jsx
@@ -35,9 +35,9 @@ const ListCompon = ({searchedData,data,delet,updateOne,items,store,quantity,upda
 
     <TextField style={{"marginTop": "12px"}} label="Search" onChange={(e,s)=>search(e,s)}/>
 
-      <List>
+      <List style={{width: window !== "undefined" ? window.innerWidth:""}}>
         {searchedData.map(e=>
-        <ListItem disablePadding>
+        <ListItem disablePadding style={{width: window !== "undefined" ? window.innerWidth:""}} >
           <Stack>
             
            {updater? <TextField id="outlined-basic" style={{width:"200px"}} label="المخزن" variant="outlined"
@@ -49,7 +49,7 @@ type="text" name="store" value={quantity} onChange={(e)=>setquantity(e.target.va
             {updater? <TextField id="outlined-basic" style={{width:"200px"}} label="الوحدة" variant="outlined"
 type="text" name="store" value={type} onChange={(e)=>settype(e.target.value)}/> : <ListItemText  >{e.types}</ListItemText>}
             
-            <Button variant="contained"   fullWidth onClick={()=>console.log(e._id)}>تحديث بيانات</Button>:<Button fullWidth color="success" variant="contained" disabled={token.isAdmin?false:true}  onClick={()=>updating(e._id,e.items,e.store,e.type,e.quantity)}>UPDATE</Button>
+            {updater?<Button variant="contained"   fullWidth onClick={()=>console.log(e._id)}>تحديث بيانات</Button>:<Button fullWidth color="success" variant="contained" disabled={token.isAdmin?false:true}  onClick={()=>updating(e._id,e.items,e.store,e.type,e.quantity)}>UPDATE</Button>}
           <Button  fullWidth color="error" variant="contained" disabled={token.isAdmin?false:true} onClick={()=>console.log("sss")}>Delete</Button>
           </Stack>
         </ListItem>)}
