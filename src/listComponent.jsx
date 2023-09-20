e8ea6a642174ee51f7b1de98fa8159907a7d336f
therealhesham <heshammoha231992@gmail.com>
2023-09-20 22:16:02 +0300

    commit on master

--- src/listComponent.jsx
+++ src/listComponent.jsx
@@ -8,11 +8,15 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
 import { Button, TextField, Stack } from '@mui/material';
+import { FixedSizeList } from 'react-window';
+import { inner } from '@cloudinary/url-gen/qualifiers/outlineMode';
 
 const ListCompon = ({searchedData,data,delet,updateOne,items,store,quantity,updater,token,search,setStore,setItems,setquantity,type,settype,updating}) => {
 
     return (<Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+
     <TextField style={{"marginTop": "12px"}} label="Search" onChange={(e,s)=>search(e,s)}/>
+<FixedSizeList height={window.innerHeight} itemSize={window.innerHeight/5} itemCount={5} width={window.innerWidth}>
     <nav aria-label="main mailbox folders">
       <List>
         {searchedData.map(e=>
@@ -27,15 +31,15 @@ type="text" name="store" value={items} onChange={(e)=>setItems(e.target.value)}/
 type="text" name="store" value={quantity} onChange={(e)=>setquantity(e.target.value)}/> : <ListItemText  >{e.quantity}</ListItemText>}
             {updater? <TextField id="outlined-basic" style={{width:"200px"}} label="الوحدة" variant="outlined"
 type="text" name="store" value={type} onChange={(e)=>settype(e.target.value)}/> : <ListItemText  >{e.types}</ListItemText>}
-            <ListItemText  >Delete</ListItemText>
-            <Button variant="contained"  style={{width:"70px"}} onClick={()=>console.log(e._id)}>تحديث بيانات</Button>:<Button color="success" variant="contained" disabled={token.isAdmin?false:true}  onClick={()=>updating(e._id,e.items,e.store,e.type,e.quantity)}>UPDATE</Button>
-          <Button style={{width:"70px"}} color="error" variant="contained" disabled={token.isAdmin?false:true} onClick={()=>console.log("sss")}>Delete</Button>
+            
+            <Button variant="contained"   fullWidth onClick={()=>console.log(e._id)}>تحديث بيانات</Button>:<Button fullWidth color="success" variant="contained" disabled={token.isAdmin?false:true}  onClick={()=>updating(e._id,e.items,e.store,e.type,e.quantity)}>UPDATE</Button>
+          <Button  fullWidth color="error" variant="contained" disabled={token.isAdmin?false:true} onClick={()=>console.log("sss")}>Delete</Button>
           </Stack>
         </ListItem>)}
       </List>
       <Divider />
     </nav>
-    
+    </FixedSizeList>
   </Box> );
 }
  
