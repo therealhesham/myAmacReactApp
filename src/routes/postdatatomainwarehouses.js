96f248795cd0b84d70e1c98844779e157be9eca6
therealhesham <68483818+therealhesham@users.noreply.github.com>
2024-08-08 09:18:45 +0300

    Update postdatatomainwarehouses.js

--- src/routes/postdatatomainwarehouses.js
+++ src/routes/postdatatomainwarehouses.js
@@ -19,7 +19,7 @@ const [success,setSuccess]= useState()
 const [stores,setStores]=useState([])
 const clear=()=>{
     setItems("")
-    // setStore("")
+    setStore("مخزن بني مزار الرئيسي")
     setType("")
     setQuantity("")
     setError(null)
